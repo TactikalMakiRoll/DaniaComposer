746c0a843e4a5e461801e8f88c85e568c0bce3d3
Oleksandr <sagridin@gmail.com>
2025-10-19 02:34:03 +0200

    remove slider 2

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,24 +5,7 @@ import leoniDiSicilia from "../assets/I Leoni di Sicilia.jpg";
 import superLocoWorld from "../assets/super loco world.png";
 import hollowHome from "../assets/hollow home.png";
 
-import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-
-const responsive = {
-  desktop: {
-    breakpoint: { max: 2560, min: 1024 },
-    items: 3,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-    slidesToSlide: 1, // optional, default to 1.
-  },
-};
 
 const Projects: React.FC = () => {
   return (
